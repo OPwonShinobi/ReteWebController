6494e1698f5c0609dd07e5373b779226f753768f
Alex Xia <xiaalex1998@gmail.com>
2022-05-10 11:09:27 -0700

    Fixed nanohttp hosted index page broken due to missing bundle.js. Now hosts all files from dist folder using their filename

--- serverlink/src/main/java/App.java
+++ serverlink/src/main/java/App.java
@@ -1,17 +1,24 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import fi.iki.elonen.NanoHTTPD;
 
 public class App extends NanoHTTPD {
   private static final int PORT = 8080;
-  private static final String HOMEPAGE = "../dist/index.html";
-  private static String homepageContents;
+  private static final String DIST = "./dist";
+  private static final String HOMEPAGE = "index.html";
+  private static Map<String, String> resources;
 
-  public static void parseHomepage() throws IOException {
-    byte[] encoded = Files.readAllBytes(Paths.get(HOMEPAGE));
-    homepageContents = new String(encoded);
+  public static void parseResources() throws IOException {
+    resources = new HashMap<>();
+    for (File file : new File(DIST).listFiles()) {
+      byte[] encoded = Files.readAllBytes(file.toPath());
+      String savedName = file.getName().equals(HOMEPAGE) ? HOMEPAGE : file.getName();
+      resources.put(savedName, new String(encoded));
+    }
   }
 
   public App(final int port) throws IOException {
@@ -26,7 +33,7 @@ public class App extends NanoHTTPD {
       port = Integer.parseInt(args[1]);
     }
     try {
-      parseHomepage();
+      parseResources();
       new App(port);
     } catch (IOException ioe) {
       System.err.println("Couldn't start server:\n" + ioe);
@@ -35,7 +42,16 @@ public class App extends NanoHTTPD {
 
   @Override
   public Response serve(IHTTPSession session) {
-    //session.getParms()
-    return newFixedLengthResponse(homepageContents);
+    String resource = session.getUri().substring(1); //trim leading "/"
+    if (resource.isEmpty()) {
+      return newFixedLengthResponse(resources.get(HOMEPAGE));
+    }
+    if (session.getMethod() == Method.GET) {
+      if (resources.get(resource) != null) {
+        return newFixedLengthResponse(resources.get(resource));
+      }
+      return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "No GET resource: " + session.getUri());
+    }
+    return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "No resource: " + session.getUri());
   }
 }

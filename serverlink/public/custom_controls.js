5bcd9407c88aba330b9c2a944ba4b5e4c40113f4
Alex Xia <xiaalex1998@gmail.com>
2022-08-08 01:07:24 -0700

    Minor quality of life change. Now pressing enter on any node with an open dropdown control will deselect the node (without this the node is locked to cursor and moves along with cursor).

--- serverlink/public/custom_controls.js
+++ serverlink/public/custom_controls.js
@@ -215,7 +215,7 @@ export class DropdownControl extends Rete.Control {
   constructor(emitter, key, selected, configType, callBackFunc) {
     super(key);
     this.emitter = emitter;
-    this.template =`<select :value="selected" @change="onChange($event)" @pointermove.stop="">`;
+    this.template =`<select :value="selected" @change="onChange($event)" @pointermove.stop="" @keyup.enter="optionClicked($event)">`;
     this.callBack = callBackFunc;
     this.scope = {
       selected,
